a169bef6a70ca1f5363d3bd56be21c99f6c512c1
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-09-29 09:07:08 -0600

    calling code from another module

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Opt())
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
